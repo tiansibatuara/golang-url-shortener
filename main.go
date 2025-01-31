27ea3c652f1f35ea746cde9821e39e75559139dd
tiansibatuara <sibatuarachristian@gmail.com>
2025-01-31 14:40:33 +0700

    init and db conn

--- main.go
+++ main.go
@@ -1,40 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"url_shortener/config"
-	"url_shortener/handlers"
-	"url_shortener/models"
-	"url_shortener/repository"
-
 	"github.com/gin-gonic/gin"
+	"github.com/tiansibatuara/golang-url-shortener/initializers"
 )
 
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDB()
+}
+
 func main() {
-	fmt.Println("Hello, World!")
-
-	//Conn to Postgres
-	db, err := config.ConnectDB()
-	if err != nil {
-		panic("Failed to connect to database")
-	}
-
-	//Auto migrate
-	db.AutoMigrate(&models.Url{})
-
-	//Init repo and handlers
-	repo := repository.NewURLRepository(db)
-	handler := handlers.NewUrlHandler(repo)
-
-	//Router
 	r := gin.Default()
-
-	r.POST("/shorten", handler.CreateShortURL)
-	r.GET("/shorten/:shortCode", handler.GetOriginalURL)
-    r.PUT("/shorten/:shortCode", handler.UpdateShortURL)
-    r.DELETE("/shorten/:shortCode", handler.DeleteShortURL)
-    r.GET("/shorten/:shortCode/stats", handler.GetStats)
-    
-
-	r.Run(":8080")
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
